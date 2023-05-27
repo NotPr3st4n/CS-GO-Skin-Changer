6304e68462cc0b562ca1ba7823ca5befffec2b5b
NotPr3st4n <121404140+NotPr3st4n@users.noreply.github.com>
2023-05-27 16:01:08 +0300

    Add files via upload

---
+++ SkinChanger/SkinChanger/main.cpp
@@ -0,0 +1,97 @@
+#include "memory.h"
+
+#include <thread>
+#include <array>
+
+namespace offset
+{
+	// client
+	constexpr ::std::ptrdiff_t dwLocalPlayer = 0xDE997C;
+	constexpr ::std::ptrdiff_t dwEntityList = 0x4DFEF0C;
+	constexpr ::std::ptrdiff_t dwClientState = 0x59F19C;
+
+	// player
+	constexpr ::std::ptrdiff_t m_hMyWeapons = 0x2E08;
+
+	// base attributable
+	constexpr ::std::ptrdiff_t m_flFallbackWear = 0x31E0;
+	constexpr ::std::ptrdiff_t m_nFallbackPaintKit = 0x31D8;
+	constexpr ::std::ptrdiff_t m_nFallbackSeed = 0x31DC;
+	constexpr ::std::ptrdiff_t m_nFallbackStatTrak = 0x31E4;
+	constexpr ::std::ptrdiff_t m_iItemDefinitionIndex = 0x2FBA;
+	constexpr ::std::ptrdiff_t m_iItemIDHigh = 0x2FD0;
+	constexpr ::std::ptrdiff_t m_iEntityQuality = 0x2FBC;
+	constexpr ::std::ptrdiff_t m_iAccountID = 0x2FD8;
+	constexpr ::std::ptrdiff_t m_OriginalOwnerXuidLow = 0x31D0;
+}
+
+// set skins to apply here
+constexpr const int GetWeaponPaint(const short& itemDefinition)
+{
+	switch (itemDefinition)
+	{
+	case 1: return 37; // deagle
+	case 4: return 680; // glock
+	case 7: return 255; // ak
+	case 9: return 662; // awp
+	case 61: return 653; // usp
+	case 28: return 514; // negev
+	case 11: return 294; // G3SG1
+	case 38: return 391; // SCAR20
+	case 19: return 636; // P90
+	case 30: return 644; // TEC9
+	case 39: return 3; // SG556
+	case 40: return 361; // SSG08
+	case 60: return 430; // M4A1-S
+	default: return 0;
+	}
+}
+
+int main()
+{
+	const auto memory = Memory{ "csgo.exe" };
+
+	// get our module addresses
+	const auto client = memory.GetModuleAddress("client.dll");
+	const auto engine = memory.GetModuleAddress("engine.dll");
+
+	// hack loop
+	while (true)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(2));
+
+		const auto& localPlayer = memory.Read<std::uintptr_t>(client + offset::dwLocalPlayer);
+		const auto& weapons = memory.Read<std::array<unsigned long, 8>>(localPlayer + offset::m_hMyWeapons);
+
+		// local player weapon iteration
+		for (const auto& handle : weapons)
+		{
+			const auto& weapon = memory.Read<std::uintptr_t>((client + offset::dwEntityList + (handle & 0xFFF) * 0x10) - 0x10);
+
+			// make sure weapon is valid
+			if (!weapon)
+				continue;
+
+			// see if we want to apply a skin
+			if (const auto paint = GetWeaponPaint(memory.Read<short>(weapon + offset::m_iItemDefinitionIndex)))
+			{
+				const bool shouldUpdate = memory.Read<std::int32_t>(weapon + offset::m_nFallbackPaintKit) != paint;
+
+				// force weapon to use fallback values
+				memory.Write<std::int32_t>(weapon + offset::m_iItemIDHigh, -1);
+
+				memory.Write<std::int32_t>(weapon + offset::m_nFallbackPaintKit, paint);
+				memory.Write<float>(weapon + offset::m_flFallbackWear, 0.1f);
+
+				//memory.Write<std::int32_t>(weapon + offset::m_nFallbackSeed, 0);
+				//memory.Write<std::int32_t>(weapon + offset::m_nFallbackStatTrak, 1337);
+				//memory.Write<std::int32_t>(weapon + offset::m_iAccountID, memory.Read<std::int32_t>(weapon + ofsset::m_OriginalOwnerXuidLow));
+
+				if (shouldUpdate)
+					memory.Write<std::int32_t>(memory.Read<std::uintptr_t>(engine + offset::dwClientState) + 0x174, -1);
+			}
+		}
+	}
+
+	return 0;
+}
